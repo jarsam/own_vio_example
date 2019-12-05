7fe3a66534deec7eddfb89c8fdd36ef098d5c956
trygas <353184965@qq.com>
2019-12-05 13:27:23 +1200

    some adds

--- PinholeCamera.cpp
+++ PinholeCamera.cpp
@@ -10,5 +10,5 @@ void PinholeCamera::LiftProjective(const Eigen::Vector2d& p, Eigen::Vector3d& P)
     double pre_distort_x = _inv_K11 * p(0) + _inv_K13;
     double pre_distort_y = _inv_K22 * p(1) + _inv_K23;
 
-    
+
 }
